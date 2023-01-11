d3db6f0148b06b8466f02f719eebe27339460f97
Alexey <alex_merser_00@internet.ru>
2023-01-11 21:55:20 +0300

    work with controller, model, presentation

--- src/main/java/ru/karpov/springcourse/config/controllers/FirstController.java
+++ src/main/java/ru/karpov/springcourse/config/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.karpov.springcourse.config.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +12,35 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("I love " + name + " " + surname);
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
+    }
+    @GetMapping("/calculator")
+    public String calculateNumbers(@RequestParam(value = "a") int a,
+                                   @RequestParam(value = "b") int b,
+                                   @RequestParam(value = "action") String action,
+                                   Model model) {
+        double result = 0;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                result = (double)a / b;
+                break;
+        }
+        model.addAttribute("result", result);
+        return "first/calculator";
     }
 
     @GetMapping("/goodbye")
